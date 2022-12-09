ae858f1e1927e8ae1b086b0cecff75debc605cf3
Cutls <web-pro@cutls.com>
2022-12-10 01:15:26 +0900

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -14,10 +14,10 @@ async function main() {
     try {
         const raw = await axios.get(`/.netlify/functions/title?url=${target}`)
         const title = raw.data.title || ''
-        const url = `http://twitter.com/share?url=${encodeURI(target)}&text=${encodeURI(title)}`
+        const url = `http://twitter.com/share?url=${encodeURIComponent(target)}&text=${encodeURIComponent(title)}`
         a.setAttribute('href', url)
     } catch (e) {
-        const url = `http://twitter.com/share?url=${encodeURI(target)}`
+        const url = `http://twitter.com/share?url=${encodeURIComponent(target)}`
         a.setAttribute('href', url)
     }
 }
